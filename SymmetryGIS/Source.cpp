4c4e9de9b9414cd83fb9922ed48da23315ed71d8
Pecniks <saso.pecnik@gmail.com>
2022-02-22 02:17:16 +0100

    WIP: - converting file from las to pc - starting symmetry detection

--- SymmetryGIS/Source.cpp
+++ SymmetryGIS/Source.cpp
@@ -8,118 +8,6 @@
 #pragma comment (lib, "UxTheme.lib")
 using namespace GemmaFusion;
 
-/**
- * @brief Georeferenced canvas helpers
-*/
-struct CanvasHelper
-{
-	CanvasHelper(Camera camera, WorldGeoreferencer* georeferencer, ShapeDrawing drawing)
-		: m_Camera(camera)
-		, m_Georeferencer(georeferencer)
-		, m_ShapeDrawing(drawing)
-	{
-	}
-
-	void PanToPoint(const Point3d& point)
-	{
-		BoundingBox2d bb{point - 10, point + 10};
-		SetViewport(bb, true);
-	}
-	void ZoomToShapeLayer(ShapeLayer layer, bool noZoom)
-	{
-		auto bb = layer->GetBoundingBox();
-		if(bb.Max >= bb.Min)
-		{
-			SetViewport(bb, noZoom);
-		}
-	}
-	void ZoomToShapeObject(ShapeObject pObject, bool noZoom)
-	{
-		BoundingBox2d bb;
-		if(pObject->CountPoints(PointCountType::Both) > 0)
-		{
-			pObject->GetBoundingBox(bb);
-			if(bb.Min == bb.Max)
-			{
-				noZoom = true;
-			}
-			SetViewport(bb, noZoom);
-		}
-	}
-	void ZoomToBoundingBox(const BoundingBox2d& bb)
-	{
-		if(bb.Max >= bb.Min)
-		{
-			SetViewport(bb, false);
-		}
-	}
-
-	/**
-	 * @brief Recenter camera to the current location
-	*/
-	void RecenterGrid()
-	{
-		Point3d pt{0.0, 0.0, 0.0};
-		auto pos = m_Camera->GetPosition2();
-		double z = pos.Z;
-
-		pt = m_Georeferencer->WorldToGeoreferencedCoordinates(pos);
-		pt.Z = 0;
-
-		m_Georeferencer->SetBasePoint(pt);
-		m_Camera->SetPosition(0, 0, -512);
-		m_Camera->SetZ(static_cast<float>(z));
-		m_ShapeDrawing->Redraw();
-	}
-
-	/**
-	 * @brief Get georeferenced location at camera center
-	 * @return
-	*/
-	Point3d GetCameraGeoreferencedCoordinates()
-	{
-		auto cameraPosition = m_Camera->GetPosition2();
-		auto georeferencedPosition = m_Georeferencer->WorldToGeoreferencedCoordinates(cameraPosition);
-		return georeferencedPosition;
-	}
-
-private:
-
-	void SetViewport(const BoundingBox2d& GeoreferencedBoundingBox, bool noZoom)
-	{
-		Point3d lt = {GeoreferencedBoundingBox.Min.X, GeoreferencedBoundingBox.Max.Y, 0.0f};
-		Point3d rb = {GeoreferencedBoundingBox.Max.X, GeoreferencedBoundingBox.Min.Y, 0.0f};
-
-		Point3d wlt;
-		Point3d wrb;
-
-		m_Georeferencer->GeoreferencedToWorldCoordinates(&lt, &wlt);
-		m_Georeferencer->GeoreferencedToWorldCoordinates(&rb, &wrb);
-
-		float cz = 0.0f;
-		if(noZoom)
-		{
-			cz = m_Camera->GetPosition().z;
-		}
-
-		GF::Renderers::GraphicsEngine::RECTD rc = {wlt.X, wlt.Y, wrb.X, wrb.Y};
-		m_Camera->SetViewportOrthogonal(rc);
-
-		if(noZoom)
-		{
-			m_Camera->SetZ(cz); // GetPositionLocal().z = cz;
-		}
-		m_Camera->Update();
-
-		RecenterGrid();
-	}
-
-
-	Camera m_Camera;
-	Core::Ptr<WorldGeoreferencer> m_Georeferencer;
-	ShapeDrawing m_ShapeDrawing;
-};
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 {
 	//
@@ -143,18 +31,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	InitializeFrequent();												// Init legacy STEZA "security" layer which decodes commonly used constants used by camera and renderers (DEV MODE)
 
 	ApplicationInstance applicationInstance(hInstance);	// Current application instance with event loop
-	Window mainWindow(L"Symmetry GIS", 1300, 700);		// Main window
-	
+		
 	// Init main spatial projection and transformations
 	Epsg epsgStore;
 	Crs wgs84 = epsgStore.GetCrs(4326);
-	Crs utm32n = epsgStore.GetCrs(3794);
-	Crs webMercator = epsgStore.GetCrs(3857);
-	CrsTransformation geographicTransform(utm32n, wgs84);
-	CrsTransformation projectedTransform(wgs84, utm32n);
+	Crs eps3794 = epsgStore.GetCrs(3794);
+	CrsTransformation geographicTransform(eps3794, wgs84);
+	CrsTransformation projectedTransform(wgs84, eps3794);
 
 	GdiPlusInstance gdiPlusInstance; // Init GDI+ (Required for png, jpg which are primarly used in MapLayerRenderer)
 	
+	Window mainWindow(L"Symmetry GIS", 1300, 700);		// Main window
 	//
 	// Init UI 
 	// 
@@ -171,7 +58,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	
 	// Status bar
 	StatusBar statusBar(mainWindow->GenerateCmdId(), mainWindow);
-	statusBar->AddLabel("projection", 350, std::format("🌐 EPSG:{} ({})", utm32n.GetEpsg(), utm32n.GetName()));
+	//statusBar->AddLabel("projection", 350, std::format("🌐 EPSG:{} ({})", utm32n.GetEpsg(), utm32n.GetName()));
+	statusBar->AddLabel("projection", 200, std::format("🌐 EPSG:{}\n({})", eps3794.GetEpsg(), eps3794.GetName()));
+	statusBar->AddLabel("coordinatesIcon", 20, "📍");
+	statusBar->AddLabel("coordinates", 140, "CX");
+	statusBar->AddLabel("longlat", 140, "GX");
+	statusBar->AddLabel("zoom", 100, "Zoom");
 
 	// Prepare main layout for window (binds WM_SIZE handler to the main window)
 	StackLayout mainLayout(mainWindow, LayoutType::Vertical);
@@ -180,7 +72,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	});
 	mainLayout->AddLayout(ribbonLayout); // Top ribbon
 	mainLayout->AddControl(canvas);  // Canvas will use rest of the free space
-	mainLayout->AddControl(statusBar, SizeType::FixedDips, 25); // Bottom control (statusbar) will have height of 25 dips
+	mainLayout->AddControl(statusBar, SizeType::FixedDips, 50); // Bottom control (statusbar) will have height of 25 dips
 
 
 
@@ -214,7 +106,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	// Setup georeferencer (transforms georeferenced points into 3D world space)
 	auto georeferencer = Core::make_ptr<WorldGeoreferencer>(camera);
 	georeferencer->SetBaseScale({ 0.5, -0.5, -0.5 });
-	georeferencer->SetBasePoint({ 1741650, 5870027 });
+	//georeferencer->SetBasePoint({ 1741650, 5870027 });
 
 	// Prepare shape renderer
 	auto shapeRenderer = Core::make_ptr<ShapeRenderer>(deviceD3D9, camera, georeferencer);
@@ -222,7 +114,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	// Prepare ShapeLayer container and assign renderers. ShapeLayer is automatically added to all renderers when inserted into a drawing
 	ShapeDrawing shapeDrawing;
 	shapeDrawing->Initialize({ shapeRenderer });
-	shapeDrawing->SetTargetCrs(utm32n);
+	shapeDrawing->SetTargetCrs(eps3794);
 	// Prepare shape loader which prepares shapelayer for rendering in the background
 	ShapeLoader shapeLoader;
 	shapeLoader->AddLayerRenderer(shapeRenderer);
@@ -233,17 +125,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	// Prepare RasterLayer container and assign renderers. RasterLayer is automatically added to all renderers when inserted into a drawing	
 	RasterDrawing rasterDrawing;
 	rasterDrawing->Initialize({ rasterRenderer });
-	rasterDrawing->SetTargetCrs(utm32n);
+	rasterDrawing->SetTargetCrs(eps3794);
 
 	// Prepare map renderer
 	MapLayerRenderer mapRenderer(deviceD3D9, camera, georeferencer);
 	mapRenderer->EnableTextureFiltering(true);
 	mapRenderer->SwapRedBlueColors(true); // 
-	mapRenderer->SetTargetCrs(utm32n);
+	mapRenderer->SetTargetCrs(eps3794);
 
 	// LasContainer is mixture of ShapeLayer and RasterLayer (uses both of them to achieve 2D rendering)
 	LasContainer lasContainer;
-	lasContainer->SetTargetCrs(utm32n);
+	lasContainer->SetTargetCrs(eps3794);
 	// Add las container shapelayer to the renderer and set it as support layer
 	ShapeLayer lasShapeLayer = lasContainer->GetLasLayer();
 	lasShapeLayer->GetStyle().FillSymbol().Show(false); // Disable shape fill
@@ -256,9 +148,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	ThreadDispatcher uiThread;
 	// Prepare thread for background processing (lidar rendering or anything else)
 	ThreadWorker processingThread("Processing thread", new_thread, Platform::Infinite());
-	
+		
 	CanvasHelper canvasHelper(camera, georeferencer, shapeDrawing);
 
+	applicationInstance->GetWorker().Post([&] { canvasHelper.PanToPoint(Geometry::Point3d(499500, 113000, 0)); });
+	applicationInstance->GetWorker().Post([&] { std::filesystem::remove_all("tmp\\"); });
+	
 	//
 	// Prepare initial data for visualization
 	//
@@ -322,24 +217,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 		return true;
 	});
 
-	//auto lastUpdateStatus = std::chrono::high_resolution_clock::now();
+	auto lastUpdateStatus = std::chrono::high_resolution_clock::now();
 	canvas->SetMouseEvent(MouseEventType::MouseMove, [&](const Mouse& mouse) {
 		auto world = camera->ScreenToWorldCoordinates(mouse.CurrentPosition2());
 		auto geo = georeferencer->WorldToGeoreferencedCoordinates(world.cast<Point3d>());
 		geo.Z = 0;
 
-		//auto updateStatus = std::chrono::high_resolution_clock::now();
-		//if(std::chrono::duration_cast<std::chrono::milliseconds>(updateStatus - lastUpdateStatus).count() > 64)
-		//{
-		//	auto latLon = geographicTransform.TransformCoordinate(geo);
+		auto updateStatus = std::chrono::high_resolution_clock::now();
+		if(std::chrono::duration_cast<std::chrono::milliseconds>(updateStatus - lastUpdateStatus).count() > 64)
+		{
+			auto latLon = geographicTransform.TransformCoordinate(geo);
 
-		//	// This file is saved as UTF-8 with BOM which means utf8 is supported by GF Apis (std::string)
-		//	statusBar->SetItemText("coordinates", Core::String::Format("%.2f m\n%.2f m", geo.X, geo.Y));
-		//	statusBar->SetItemText("longlat", Core::String::Format("%f°\n%f°", latLon.X, latLon.Y));
-		//	statusBar->SetItemText("zoom", Core::String::Format("🔎 %.2f", abs(camera->GetPosition2().Z)));
+			// This file is saved as UTF-8 with BOM which means utf8 is supported by GF Apis (std::string)
+			statusBar->SetItemText("coordinates", Core::String::Format("%.2f m\n%.2f m", geo.X, geo.Y));
+			statusBar->SetItemText("longlat", Core::String::Format("%f°\n%f°", latLon.X, latLon.Y));
+			statusBar->SetItemText("zoom", Core::String::Format("🔎 %.2f", abs(camera->GetPosition2().Z)));
 
-		//	lastUpdateStatus = updateStatus;
-		//}
+			lastUpdateStatus = updateStatus;
+		}
 		shapeRenderer->Update(true);
 	});
 
@@ -359,12 +254,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 
 	canvas->SetMouseWheelEvent([&](const Mouse& mouse, int zDelta) {
 		shapeRenderer->Update(true);
-		//auto updateStatus = std::chrono::high_resolution_clock::now();
-		//if(std::chrono::duration_cast<std::chrono::milliseconds>(updateStatus - lastUpdateStatus).count() > 64)
-		//{
-		//	statusBar->SetItemText("zoom", Core::String::Format("🔎 %.2f", abs(camera->GetPosition2().Z)));
-		//	lastUpdateStatus = updateStatus;
-		//}
+		auto updateStatus = std::chrono::high_resolution_clock::now();
+		if(std::chrono::duration_cast<std::chrono::milliseconds>(updateStatus - lastUpdateStatus).count() > 64)
+		{
+			statusBar->SetItemText("zoom", Core::String::Format("🔎 %.2f", abs(camera->GetPosition2().Z)));
+			lastUpdateStatus = updateStatus;
+		}
 	});
 
 	mainWindow->AddEventHandler(WM_CLOSE, [&](HWND hWnd, WPARAM wParam, LPARAM lParam, LRESULT& lResult)
@@ -382,6 +277,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	// Button click
 	ribbon->SetOnCommandExecute(ID_EXIT, [&]()
 	{
+		std::filesystem::remove_all("tmp\\");
 		//APPRESULT_DEV_INFORMATION("Exit");
 		applicationInstance->Exit(0);
 	});
@@ -421,36 +317,44 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 		}
 	});
 
-	ribbon->SetOnCommandExecute(ID_LOADDATA, [&]{
-		APPRESULT_DEV_INFORMATION("Load Data");
-	FilePicker picker;
-	//auto las = picker.AddExtensionFilter("LiDAR files", "*.las");
-	//if(auto result = picker.OpenMultipleDialog("Open lidar files"))
-	//{
-	//	std::vector<ShapeObject> added;
-	//	std::vector<Utils::Plane> planes;
-	//	for(auto& file : result.value().Path())
-	//	{
-	//		added.push_back(lasContainer->AddLas(file));
+	ribbon->SetOnCommandExecute(ID_LOADDATA, [&] {
+		//APPRESULT_DEV_INFORMATION("Load Data");
+		FilePicker picker;
+		auto las = picker.AddExtensionFilter("LiDAR files", "*.las");
+		if (auto result = picker.OpenMultipleDialog("Open lidar files"))
+		{
+			std::vector<ShapeObject> added;
+			//	std::vector<Utils::Plane> planes;
+			for (auto& file : result.value().Path())
+			{
+				added.push_back(lasContainer->AddLas(file));
 
-	//		// create point cloud from las files
-	//		auto args = fmt::format("las2txt.exe -i {} --column-delimiter \" \" --line-format x y z -o tmp\\", file);
-	//		auto processInstance = Platform::Windows::Process::CreateProcessInstance("las2txt\\las2txt.exe", args);
-	//		processInstance->Start();
-	//		processInstance->Await();
-	//		auto pos = file.find_last_of("\\");
-	//		std::string oldf(file, pos + 1); oldf.insert(0, "tmp\\"); oldf.replace(oldf.size() - 3, 3, "txt");
-	//		std::string newf(file, pos + 1); newf.insert(0, "tmp\\"); newf.replace(newf.size() - 3, 3, "pc");
-	//		std::filesystem::rename(oldf.c_str(), newf.c_str());
+				// create point cloud from las files
+				auto args = fmt::format("las2txt.exe -i {} --column-delimiter \" \" --line-format x y z --overwrite-files 1 --file-extension pc -o tmp\\", file);
+				APPRESULT_DEV_INFORMATION("%s", args);
+				auto pi = Platform::Windows::Process::CreateProcessInstance("las2txt\\las2txt.exe", args);// , Platform::Interfaces::StandardIO::RedirectInputOutput);
+				pi->Start();
+				pi->Await();
+				//std::string output = Platform::Windows::Process::GetProcessOutput(pi);
+				
+				auto pos = file.find_last_of("\\");
+				std::string pcFile(file, pos + 1); pcFile.insert(0, "tmp\\"); pcFile.replace(pcFile.size() - 3, 3, "pc");
+				APPRESULT_DEV_INFORMATION("%s", pcFile);
 
 
-	//		auto argsym = fmt::format("SymmetryDetector.exe {} {}", newf, false);
-	//		auto processInstanceSym = Platform::Windows::Process::CreateProcessInstance("SymmetryDetector\\SymmetryDetector.exe", argsym);
-	//		processInstanceSym->Start();//std::format()
-	//		processInstanceSym->Await();
-	//		Utils::Plane plane;
-	//		readSymetrieFile(newf + ".txt", plane);
-	//		planes.push_back(plane);
+				auto argsym = fmt::format("SymmetryDetector.exe {} {}", pcFile, false); 
+				APPRESULT_DEV_INFORMATION("%s", argsym);
+				auto piSym = Platform::Windows::Process::CreateProcessInstance("SymmetryDetector\\SymmetryDetector.exe", argsym, Platform::Interfaces::StandardIO::RedirectInputOutput);
+				piSym->Start();//std::format()
+				piSym->Await();
+				std::string symRes = Platform::Windows::Process::GetProcessOutput(piSym);
+				APPRESULT_DEV_INFORMATION("%s", symRes);
+
+
+
+				//Utils::Plane plane;
+				//readSymetrieFile(newf + ".txt", plane);
+				//planes.push_back(plane);
 
 	//		auto bb = added.back()->GetBoundingBox2d();
 	//		Utils::BoundingBox6f BB((float)bb.Min.X, (float)bb.Min.Y, 0.f, (float)bb.Max.X, (float)bb.Max.Y, 0.f);
@@ -470,28 +374,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmdArgs, int nShowWnd)
 	//		shapeDrawing->AddShapeLayer(lineLayer);
 	//		canvasHelper.ZoomToShapeLayer(lineLayer, false);
 	//		//applicationInstance->GetWorker().Post([&] { canvasHelper.ZoomToShapeLayer(lineLayer, false); });
-	//	}
+			}
 
-	//	// Submit lidar rendering to background thread
-	//	processingThread.Post([added, uiThread, lasContainer, rasterDrawing, l_hillshade_colormap]() mutable
-	//						{
-	//							for(auto& addedLas : added)
-	//							{
-	//								auto raster = lasContainer->CreateLasRasterLayer(addedLas);
-	//								lasContainer->GenerateLasRasterObjects(raster, 1.0, GenerateLasRasterObjectProperties(true, true, {2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}));
-	//								// Submit rendered raster to the ui thread
-	//								uiThread->Post([raster, rasterDrawing, l_hillshade_colormap]() mutable
-	//												{
-	//													raster[0]->SetCustomColorMapKey("Hillshade");
-	//													raster[0]->SetCustomColorMapHandler(l_hillshade_colormap);
-	//													rasterDrawing->AddRasterLayer(raster);
-	//												});
-	//							}
-	//						});
+			// Submit lidar rendering to background thread
+			processingThread.Post([added, uiThread, lasContainer, rasterDrawing, l_hillshade_colormap]() mutable
+			{
+				for(auto& addedLas : added)
+				{
+					auto raster = lasContainer->CreateLasRasterLayer(addedLas);
+					lasContainer->GenerateLasRasterObjects(raster, 1.0, GenerateLasRasterObjectProperties(true, true, {2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}));
+					// Submit rendered raster to the ui thread
+					uiThread->Post([raster, rasterDrawing, l_hillshade_colormap]() mutable
+									{
+										raster[0]->SetCustomColorMapKey("Hillshade");
+										raster[0]->SetCustomColorMapHandler(l_hillshade_colormap);
+										rasterDrawing->AddRasterLayer(raster);
+									});
+				}
+			});
 
-	//	//canvasHelper.ZoomToBoundingBox(added.at(0)->GetBoundingBox2d());
+			canvasHelper.ZoomToBoundingBox(added.at(0)->GetBoundingBox2d());
 
-	//}
+		}
 	});
 
 	//
